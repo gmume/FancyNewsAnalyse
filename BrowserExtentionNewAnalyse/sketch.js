55fc64267496e66013a9f16f44438819d07f679b
thomas-schaad <132581101+thomas-schaad@users.noreply.github.com>
2023-05-23 15:40:23 +0200

    Anzahl Zeichen
    Lorem Ipsum Text in abhängigkeit aller Zeichen der aktuellen Page

--- BrowserExtentionNewAnalyse/sketch.js
+++ BrowserExtentionNewAnalyse/sketch.js
@@ -1,26 +1,103 @@
-console.log("Hello World!");
 
-const sketch = function (p5) {
-  p5.setup = function () {
+  let alleBuchstabenSum;
+
+  const sketch = function(p5) {
+
+  let textContent = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ame";
+  let textArray = [];
+
+  p5.setup = function() {
+    for (let i = 0; i < textContent.length; i++) {
+      let index = i % 3000;
+      textArray[index] = textContent.charAt(i);
+    }
+
     p5.rectMode(p5.CENTER);
+
     let c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
     c.style("top", "0px");
     c.style("left", "0px");
     c.style("pointer-events", "none");
     c.style("position", "fixed");
     c.style("z-index", "99999999");
-    p5.clear();
+
+    let h2Tags = document.getElementsByTagName("h2");
+    let h2LetterCountSum = 0;
+
+    for (let i = 0; i < h2Tags.length; i++) {
+      let h2Tag = h2Tags[i];
+      let h2Text = h2Tag.textContent;
+      let h2LetterCount = h2Text.length;
+      h2LetterCountSum += h2LetterCount;
+    }
+
+    let h1Tags = document.getElementsByTagName("h1");
+    let h1LetterCountSum = 0;
+
+    for (let i = 0; i < h1Tags.length; i++) {
+      let h1Tag = h1Tags[i];
+      let h1Text = h1Tag.textContent;
+      let h1LetterCount = h1Text.length;
+      h1LetterCountSum += h1LetterCount;
+    }
+
+    let aTags = document.getElementsByTagName("a");
+    let aLetterCountSum = 0;
+
+    for (let i = 0; i < aTags.length; i++) {
+      let aTag = aTags[i];
+      let aText = aTag.textContent;
+      let aLetterCount = aText.length;
+      aLetterCountSum += aLetterCount;
+    }
+
+    let pTags = document.getElementsByTagName("p");
+    let pLetterCountSum = 0;
+
+    for (let i = 0; i < pTags.length; i++) {
+      let pTag = pTags[i];
+      let pText = pTag.textContent;
+      let pLetterCount = pText.length;
+      pLetterCountSum += pLetterCount;
+    }
+
+    alleBuchstabenSum = h2LetterCountSum + h1LetterCountSum + aLetterCountSum + pLetterCountSum;
+    alleBuchstabenSum = Math.min(alleBuchstabenSum, 30000);
+
+    console.log('Anzahl Buchstaben H1 ' + h1LetterCountSum);
+    console.log('Anzahl Buchstaben H2 ' + h2LetterCountSum);
+    console.log('Anzahl Buchstaben A ' + aLetterCountSum);
+    console.log('Anzahl Buchstaben P ' + pLetterCountSum);
+    console.log("Alle Buchstaben: " + alleBuchstabenSum);
   };
 
-  p5.draw = function () {
-    //p5.clear();
-    p5.fill(p5.random(0, 255), p5.random(0, 255), p5.random(0, 255));
-    p5.rect(p5.mouseX, p5.mouseY, 50, 50, p5.random(0, 25));
-  };
+  p5.draw = function() {
+    p5.textSize(20);
+    p5.fill(255, 0, 0);
+    p5.textFont("Arial");
 
-  p5.windowResized = function () {
+    let x = 50;
+    let y = 100;
+
+    for (let i = 0; i < textArray.length; i++) {
+    p5.text(textArray[i], x, y);
+
+    if (x + p5.textWidth(textArray[i]) >= p5.windowWidth - 50) {
+    x = 50;
+    y += 25;
+    } else {
+    x += p5.textWidth(textArray[i]);
+    }
+
+    if (i >= (alleBuchstabenSum / 30000) * textContent.length) {
+    break;
+    }
+    }
+    };
+
+    p5.windowResized = function() {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
   };
 };
 
-let my_scetch = new p5(sketch);
+let my_sketch = new p5(sketch);
