2feb40f25b2d4d8f4d0debf7740967f3ec23a02d
vanessarenggli <vanessa.renggli2@gmail.com>
2023-05-26 11:53:27 +0200

    Fonts turn 90 Degrees

--- BrowserExtentionNewAnalyse/FontsUsed.js
+++ BrowserExtentionNewAnalyse/FontsUsed.js
@@ -35,23 +35,25 @@ class FontsUsed {
         let y = 100;
 
         for (let i = 0; i < this.fonts.length; i++) {
+
+            this.p5.push();
+            this.p5.translate(this.p5.width / 2, this.p5.height / 2);
+            this.p5.angleMode(this.p5.DEGREES);
+            let angle = -90;
+            this.p5.rotate(angle);
+            this.p5.textSize(50);
+            this.p5.noStroke();
+            this.p5.fill("blue");
+
             this.p5.textFont(this.fonts[i]);
-            this.p5.text(this.fonts[i], 50, y);
+            this.p5.text(this.fonts[i], 0, y);
+
+            this.p5.pop();
+
             y += 50;
         }
 
         // Fonts turn 90 degrees
-        this.p5.push();
-        this.p5.translate(this.p5.width - 150, this.p5.height / 2);
-        this.p5.angleMode(this.p5.DEGREES);
-        let angle = -90;
-        this.p5.rotate(angle);
-        this.p5.textSize(50);
-        this.p5.noStroke();
-        this.p5.fill("blue");
-        this.p5.text("Schriftart 1", 0, 0);
-        this.p5.text("Schriftart 2", 0, 50);
-        this.p5.text("Schriftart 3", 0, 100);
-        this.p5.pop();
+
     }
 }
