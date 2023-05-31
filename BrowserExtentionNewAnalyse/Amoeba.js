b135669c64d1c682391ac702417772e6ac9527b8
timohaenni <timo.haenni@hotmail.com>
2023-05-31 15:52:49 +0200

    second amoeba added but gray has to be fixed

--- BrowserExtentionNewAnalyse/Amoeba.js
+++ BrowserExtentionNewAnalyse/Amoeba.js
@@ -13,17 +13,16 @@ class Amoeba {
     this.angleGap;
     this.colScale;
     this.firstPoint;
-    this.colCenterX = 0;
+    this.colCenterX = -300;
     this.colCenterY = 0;
     this.colTheta = 0;
-    this.grayCenterX = 1000;
-    this.grayCenterY = 300;
+    this.grayCenterX = 300;
+    this.grayCenterY = 0;
     this.grayTheta = 0;
     this.scale;
     this.colorsCount = [];
 
-
-    for(let i = 0; i<this.colors.length; i++){
+    for (let i = 0; i < this.colors.length; i++) {
       //console.log(i);
       this.colorsCount.push(i);
     }
@@ -40,7 +39,6 @@ class Amoeba {
 
     this.cWEBGL.setAttributes("alpha", true);
     this.cWEBGL.angleMode(this.p5.DEGREES);
-    this.cWEBGL.colorMode(this.cWEBGL.HSB, 360, 100, 100, 100);
     this.cWEBGL.noStroke();
 
     this.angleGap = 360 / this.numPoints;
@@ -48,7 +46,6 @@ class Amoeba {
       .scaleLinear()
       .domain(this.colorsCount)
       .range(this.colors);
-
   }
 
   draw() {
@@ -92,12 +89,7 @@ class Amoeba {
       nx = this.cWEBGL.sin(a); // nx and ny pos for noisy hue jitter
       ny = this.cWEBGL.cos(a);
 
-      let h = this.cWEBGL.map(nx,
-        -1,
-        1,
-        0,
-        this.colorsCount.length-1
-      );
+      let h = this.cWEBGL.map(nx, -1, 1, 0, this.colorsCount.length - 1);
       let x = this.cWEBGL.sin(a) * r; // actual x and y to draw
       let y = this.cWEBGL.cos(a) * r;
 
@@ -116,8 +108,6 @@ class Amoeba {
 
       this.cWEBGL.fill(this.colScale(h));
       this.cWEBGL.vertex(x, y);
-
-  console.log(h);
     }
 
     this.cWEBGL.fill(this.colScale(this.firstPoint.h));
@@ -138,6 +128,70 @@ class Amoeba {
     this.cWEBGL.pop();
     // this.colTheta = this.colTheta - 0.1;
 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    this.cWEBGL.push();
+    this.cWEBGL.translate(this.grayCenterX, this.grayCenterY);
+    // this.cWEBGL.rotate(colTheta);
+
+    // Main Shape
+    this.cWEBGL.beginShape();
+
+    // This color will "shine" from centre
+    //this.cWEBGL.fill(0, 0, 0);
+
+    // Draw one vertex in middle of canvas to start and end shape,
+    // so that the gradient fill will "aim" towards there instead of
+    // to one side of the shape
+    this.cWEBGL.vertex(0, 0);
+
+    for (let i = 0; i < this.numPoints; i++) {
+      let a = this.angleGap * i;
+      let nx = 600 + this.cWEBGL.sin(a) * 0.3; // nx and ny pos for noisy angle jitter
+      let ny = 600 + this.cWEBGL.cos(a) * 0.3;
+      let r =
+        this.scale(this.proportion.getTextBoxArea()) +
+        this.cWEBGL.map(this.p5.noise(nx, ny, this.zpos), 0, 1, -100, 100);
+
+      nx = this.cWEBGL.sin(a); // nx and ny pos for noisy hue jitter
+      ny = this.cWEBGL.cos(a);
+
+      let h = this.cWEBGL.map(nx, -1, 1, 0, 100);
+      let x = this.cWEBGL.sin(a) * r; // actual x and y to draw
+      let y = this.cWEBGL.cos(a) * r;
+
+      if (i == 0) {
+        this.firstPoint = {
+          x: x,
+          y: y,
+          h: h,
+        };
+      }
+
+      this.points[i] = {
+        x: this.cWEBGL.sin(a) * r * 0.2, // Saving smaller version of x and y
+        y: this.cWEBGL.cos(a) * r * 0.2, // to draw smaller inner shape in bg color
+      }; // to cover the bit where the gradient all joins together
+
+      this.cWEBGL.fill(0, 0, 50);
+      this.cWEBGL.vertex(x, y);
+    }
+
+    this.cWEBGL.fill(0, 0, 50);
+    this.cWEBGL.vertex(this.firstPoint.x, this.firstPoint.y); // First / Last outer point
+    this.cWEBGL.vertex(0, 0); // End shape in centre so gradient aims there
+    this.cWEBGL.endShape(this.cWEBGL.CLOSE);
+
+    // Inner Shape
+    this.cWEBGL.fill(this.p5.color("black"));
+    this.cWEBGL.beginShape();
+
+    for (let p of this.points) {
+      this.cWEBGL.vertex(p.x, p.y);
+    }
+
+    this.cWEBGL.endShape(this.cWEBGL.CLOSE);
+
+    this.cWEBGL.pop();
     // To animate through noise
     this.zpos += 0.003;
     this.zhue += 0.008;
