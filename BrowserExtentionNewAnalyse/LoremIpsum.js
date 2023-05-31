56140d97f474580d66af5002ca7d9aaa27a6e248
vanessarenggli <vanessa.renggli2@gmail.com>
2023-05-31 16:23:34 +0200

    Debugging mit Screenshot, Legendentext unten, Buttons neue Position und Textlänge

--- BrowserExtentionNewAnalyse/LoremIpsum.js
+++ BrowserExtentionNewAnalyse/LoremIpsum.js
@@ -1,106 +1,106 @@
 class LoremIpsum {
-  constructor(p5) {
-    this.p5 = p5;
-    this.textContent =
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ame";
-    this.textArray = this.createText();
-    this.alleBuchstabenSum = this.countChars();
-    this.charsSum;
-  }
-
-  getTextArray() {
-    return this.textArray;
-  }
-
-  getAlleBuchstabenSum() {
-    return this.alleBuchstabenSum;
-  }
-
-  createText() {
-    let loremIpsum = [];
-
-    for (let i = 0; i < this.textContent.length; i++) {
-      let index = i % 3000;
-      loremIpsum[index] = this.textContent.charAt(i);
+    constructor(p5) {
+        this.p5 = p5;
+        this.textContent =
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ame";
+        this.textArray = this.createText();
+        this.alleBuchstabenSum = this.countChars();
+        this.charsSum;
     }
 
-    return loremIpsum;
-  }
-
-  countChars() {
-    let charsSum;
-
-    let h2Tags = document.getElementsByTagName("h2");
-    let h2LetterCountSum = 0;
-
-    for (let i = 0; i < h2Tags.length; i++) {
-      let h2Tag = h2Tags[i];
-      let h2Text = h2Tag.textContent;
-      let h2LetterCount = h2Text.length;
-      h2LetterCountSum += h2LetterCount;
+    getTextArray() {
+        return this.textArray;
     }
 
-    let h1Tags = document.getElementsByTagName("h1");
-    let h1LetterCountSum = 0;
-
-    for (let i = 0; i < h1Tags.length; i++) {
-      let h1Tag = h1Tags[i];
-      let h1Text = h1Tag.textContent;
-      let h1LetterCount = h1Text.length;
-      h1LetterCountSum += h1LetterCount;
+    getAlleBuchstabenSum() {
+        return this.alleBuchstabenSum;
     }
 
-    let aTags = document.getElementsByTagName("a");
-    let aLetterCountSum = 0;
+    createText() {
+        let loremIpsum = [];
 
-    for (let i = 0; i < aTags.length; i++) {
-      let aTag = aTags[i];
-      let aText = aTag.textContent;
-      let aLetterCount = aText.length;
-      aLetterCountSum += aLetterCount;
+        for (let i = 0; i < this.textContent.length; i++) {
+            let index = i % 3000;
+            loremIpsum[index] = this.textContent.charAt(i);
+        }
+
+        return loremIpsum;
     }
 
-    let pTags = document.getElementsByTagName("p");
-    let pLetterCountSum = 0;
+    countChars() {
+        // let charsSum;
 
-    for (let i = 0; i < pTags.length; i++) {
-      let pTag = pTags[i];
-      let pText = pTag.textContent;
-      let pLetterCount = pText.length;
-      pLetterCountSum += pLetterCount;
+        let h2Tags = document.getElementsByTagName("h2");
+        let h2LetterCountSum = 0;
+
+        for (let i = 0; i < h2Tags.length; i++) {
+            let h2Tag = h2Tags[i];
+            let h2Text = h2Tag.textContent;
+            let h2LetterCount = h2Text.length;
+            h2LetterCountSum += h2LetterCount;
+        }
+
+        let h1Tags = document.getElementsByTagName("h1");
+        let h1LetterCountSum = 0;
+
+        for (let i = 0; i < h1Tags.length; i++) {
+            let h1Tag = h1Tags[i];
+            let h1Text = h1Tag.textContent;
+            let h1LetterCount = h1Text.length;
+            h1LetterCountSum += h1LetterCount;
+        }
+
+        let aTags = document.getElementsByTagName("a");
+        let aLetterCountSum = 0;
+
+        for (let i = 0; i < aTags.length; i++) {
+            let aTag = aTags[i];
+            let aText = aTag.textContent;
+            let aLetterCount = aText.length;
+            aLetterCountSum += aLetterCount;
+        }
+
+        let pTags = document.getElementsByTagName("p");
+        let pLetterCountSum = 0;
+
+        for (let i = 0; i < pTags.length; i++) {
+            let pTag = pTags[i];
+            let pText = pTag.textContent;
+            let pLetterCount = pText.length;
+            pLetterCountSum += pLetterCount;
+        }
+
+        this.charsSum = h2LetterCountSum + h1LetterCountSum + aLetterCountSum +
+            pLetterCountSum;
+        this.charsSum = Math.min(this.charsSum, 30000);
+
+        return this.charsSum;
     }
 
-    this.charsSum = h2LetterCountSum + h1LetterCountSum + aLetterCountSum +
-      pLetterCountSum;
-    this.charsSum = Math.min(this.charsSum, 30000);
-
-    return charsSum;
-  }
-
-  getCharsSum() {
-    return this.charsSum;
-  }
-
-  createTextField() {
-    let x = 400; // randabstand text links erste zeile
-    let y = 170;
-
-    for (let i = 0; i < this.textArray.length; i++) {
-      this.p5.text(this.textArray[i], x, y);
-
-      //textfeld grösse
-      if (
-        x + this.p5.textWidth(this.textArray[i]) >= this.p5.windowWidth - 400
-      ) { //300= randabstand rechts
-        x = 400; // randabstand links alle anderen zeilen
-        y += 17; // Zeilenabstand
-      } else {
-        x += this.p5.textWidth(this.textArray[i]);
-      }
-
-      if (i >= (this.alleBuchstabenSum / 30000) * this.textContent.length) {
-        break;
-      }
+    getCharsSum() {
+        return this.charsSum;
     }
-  }
+
+    createTextField() {
+        let x = 400; // randabstand text links erste zeile
+        let y = 170;
+
+        for (let i = 0; i < this.textArray.length; i++) {
+            this.p5.text(this.textArray[i], x, y);
+
+            //textfeld grösse
+            if (
+                x + this.p5.textWidth(this.textArray[i]) >= this.p5.windowWidth - 400
+            ) { //300= randabstand rechts
+                x = 400; // randabstand links alle anderen zeilen
+                y += 17; // Zeilenabstand
+            } else {
+                x += this.p5.textWidth(this.textArray[i]);
+            }
+
+            if (i >= (this.alleBuchstabenSum / 30000) * this.textContent.length) {
+                break;
+            }
+        }
+    }
 }
--- BrowserExtentionNewAnalyse/sketch.js
+++ BrowserExtentionNewAnalyse/sketch.js
@@ -17,129 +17,157 @@ let buttonLetter;
 let buttonpressedLetter = false;
 let buttonAmoeba;
 let buttonpressedAmoeba = false;
+let buttonSave;
+let buttonpressedSave = false;
 
-const sketch = function (p5) {
-  p5.setup = function () {
-    setupCanvas(p5);
+const sketch = function(p5) {
+    p5.setup = function() {
+        setupCanvas(p5);
 
-    beginingLetter = new BeginingLetter(p5);
-    loremIpsum = new LoremIpsum(p5);
-    colorManager = new Colors();
-    colorList = colorManager.getColors();
-    fontsManager = new FontsUsed(p5);
-    fontsList = fontsManager.getFonts();
-    amoeba = new Amoeba(p5, d3, new Proportion(p5), new Colors().getColors());
+        beginingLetter = new BeginingLetter(p5);
+        loremIpsum = new LoremIpsum(p5);
+        colorManager = new Colors();
+        colorList = colorManager.getColors();
+        fontsManager = new FontsUsed(p5);
+        fontsList = fontsManager.getFonts();
+        amoeba = new Amoeba(p5, d3, new Proportion(p5), new Colors().getColors());
 
-    let buttonBackground = p5.createDiv("blubb");
-    buttonBackground.position(0, p5.windowHeight - 50);
-    buttonBackground.style("background-color", "black");
-    buttonBackground.style("font-size", "50px");
-    buttonBackground.style("position", "fixed");
-    buttonBackground.style("width", "1500px");
-    buttonBackground.style("z-index", "3000");
+        let buttonBackground = p5.createDiv("blubb");
+        buttonBackground.position(0, p5.windowHeight - 50);
+        buttonBackground.style("background-color", "black");
+        buttonBackground.style("font-size", "50px");
+        buttonBackground.style("position", "fixed");
+        buttonBackground.style("width", "1500px");
+        buttonBackground.style("z-index", "3000");
 
-    buttonAll = p5.createButton("on/off");
-    buttonAll.position(1425, p5.windowHeight - 40);
-    buttonAll.mousePressed(toggleButtonAll);
-    buttonAll.style("position", "fixed");
-    buttonAll.class("buttonAll");
+        buttonAll = p5.createButton("on/off");
+        buttonAll.position(p5.windowWidth - 70, p5.windowHeight - 40);
+        buttonAll.mousePressed(toggleButtonAll);
+        buttonAll.style("position", "fixed");
+        buttonAll.class("buttonAll");
 
-    buttonNumber = p5.createButton("character count");
-    buttonNumber.position(493, p5.windowHeight - 40);
-    buttonNumber.mousePressed(toggleButtonNumber);
-    buttonNumber.style("position", "fixed");
-    buttonNumber.class("buttonNumber");
+        buttonNumber = p5.createButton("character count");
+        buttonNumber.position(493, p5.windowHeight - 40);
+        buttonNumber.mousePressed(toggleButtonNumber);
+        buttonNumber.style("position", "fixed");
+        buttonNumber.class("buttonNumber");
 
-    buttonLetter = p5.createButton("font");
-    buttonLetter.position(414, p5.windowHeight - 40);
-    buttonLetter.mousePressed(toggleButtonLetter);
-    buttonLetter.style("position", "fixed");
-    buttonLetter.class("buttonLetter");
+        buttonLetter = p5.createButton("font");
+        buttonLetter.position(414, p5.windowHeight - 40);
+        buttonLetter.mousePressed(toggleButtonLetter);
+        buttonLetter.style("position", "fixed");
+        buttonLetter.class("buttonLetter");
 
-    buttonAmoeba = p5.createButton("image-to-text ratio");
-    buttonAmoeba.position(265, p5.windowHeight - 40);
-    buttonAmoeba.mousePressed(toggleButtonAmoeba);
-    buttonAmoeba.style("position", "fixed");
-    buttonAmoeba.class("buttonAmoeba");
+        buttonAmoeba = p5.createButton("image-to-text ratio");
+        buttonAmoeba.position(265, p5.windowHeight - 40);
+        buttonAmoeba.mousePressed(toggleButtonAmoeba);
+        buttonAmoeba.style("position", "fixed");
+        buttonAmoeba.class("buttonAmoeba");
 
-    buttonSave = p5.createButton("screenshot");
-    buttonSave.position(1300, p5.windowHeight - 40);
-    buttonSave.style("position", "fixed");
-    buttonSave.class("buttonSave");
-  };
+        buttonSave = p5.createButton("screenshot");
+        buttonSave.position(p5.windowWidth - 170, p5.windowHeight - 40);
+        buttonSave.mousePressed(saveToFile);
+        buttonSave.style("position", "fixed");
+        buttonSave.class("buttonSave");
+    };
 
-  p5.draw = function () {
-    c.clear();
-
-    //show colors
-    let x = 0;
-
-    for (let i = 0; i < colorList.length; i++) {
-      p5.fill(p5.color(colorList[i]));
-      p5.rect(x, 0, 30, 30);
-      x += 30;
+    function saveToFile() {
+        buttonSave = true;
     }
 
-    if (
-      buttonpressedAll || buttonpressedNumber || buttonpressedLetter ||
-      buttonpressedAmoeba
-    ) {
-      if (!buttonpressedNumber) {
+    p5.draw = function() {
+        c.clear();
+
+        if (buttonSave == true) {
+            p5.background('white');
+        }
+
+        //show colors
+        let x = 0;
+
+        for (let i = 0; i < colorList.length; i++) {
+            p5.fill(p5.color(colorList[i]));
+            p5.rect(x, 0, 30, 30);
+            x += 30;
+        }
+
+        if (
+            buttonpressedAll || buttonpressedNumber || buttonpressedLetter ||
+            buttonpressedAmoeba
+        ) {
+            if (!buttonpressedNumber) {
+                p5.push();
+                p5.fill(0);
+                p5.textSize(15);
+                p5.textFont(fontsList[0]);
+                loremIpsum.createTextField();
+                p5.textSize(100);
+                p5.text(loremIpsum.getCharsSum(), 350, 200);
+                p5.pop();
+            }
+
+            if (!buttonpressedLetter) {
+                p5.push();
+                p5.textFont(fontsList[0]);
+                p5.fill(0);
+                p5.textSize(400);
+                p5.text(
+                    document.title.charAt(0),
+                    p5.windowWidth / 1.6,
+                    p5.windowHeight / 1.2,
+                );
+                p5.pop();
+            }
+
+            if (!buttonpressedAmoeba) {
+                //shows an amoeba
+                amoeba.draw();
+            }
+        }
+
+        if (buttonSave == true) {
+            p5.saveCanvas('dataviz', 'jpg');
+
+            buttonSave = false;
+        }
+
         p5.push();
-        p5.fill(0);
+        p5.fill('white');
         p5.textSize(15);
-        p5.textFont(fontsList[0]);
-        loremIpsum.createTextField();
-        p5.textSize(100);
-        p5.text(loremIpsum.getCharsSum(), 350, 200);
+        p5.textFont('Helvetica');
+        p5.text('This browser extension displays the', 20, p5.windowHeight - 20);
+        p5.text(',', 405, p5.windowHeight - 20);
+        p5.text('and', 460, p5.windowHeight - 20);
+        p5.text('of the visited web page.', 618, p5.windowHeight - 20);
         p5.pop();
-      }
 
-      if (!buttonpressedLetter) {
-        p5.push();
-        p5.textFont(fontsList[0]);
-        p5.fill(0);
-        p5.textSize(400);
-        p5.text(
-          document.title.charAt(0),
-          p5.windowWidth / 1.6,
-          p5.windowHeight / 1.2,
-        );
-        p5.pop();
-      }
+    };
 
-      if (!buttonpressedAmoeba) {
-        //shows an amoeba
-        amoeba.draw();
-      }
+    function toggleButtonAll() {
+        buttonpressedAll = !buttonpressedAll;
     }
-  };
 
-  function toggleButtonAll() {
-    buttonpressedAll = !buttonpressedAll;
-  }
+    function toggleButtonNumber() {
+        buttonpressedNumber = !buttonpressedNumber;
+    }
 
-  function toggleButtonNumber() {
-    buttonpressedNumber = !buttonpressedNumber;
-  }
+    function toggleButtonLetter() {
+        buttonpressedLetter = !buttonpressedLetter;
+    }
 
-  function toggleButtonLetter() {
-    buttonpressedLetter = !buttonpressedLetter;
-  }
-
-  function toggleButtonAmoeba() {
-    buttonpressedAmoeba = !buttonpressedAmoeba;
-  }
+    function toggleButtonAmoeba() {
+        buttonpressedAmoeba = !buttonpressedAmoeba;
+    }
 };
 
-const setupCanvas = function (p5) {
-  c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
-  p5.frameRate(60);
-  c.style("top", "0px");
-  c.style("left", "0px");
-  c.style("pointer-events", "none");
-  c.style("position", "fixed");
-  c.style("z-index", "99999999");
+const setupCanvas = function(p5) {
+    c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    p5.frameRate(60);
+    c.style("top", "0px");
+    c.style("left", "0px");
+    c.style("pointer-events", "none");
+    c.style("position", "fixed");
+    c.style("z-index", "99999999");
 };
 
 new p5(sketch);
